2b19aedae12f0732256f4d87b1a6e18ecbcff9e9
Jhonatan <jsjhonatanjsarroyo@gmail.com>
2022-02-02 19:02:35 -0500

    Agregue una funcion para que aparezcan guiones segun el texto

--- javascript.js
+++ javascript.js
@@ -1,3 +1,27 @@
+function texto() {
+  let letras = input.value;
+  let a = 1;
+  let b = 0;
+  let c = 44;
+  for (let i = 0; i < letras.length; i++) {
+    pizarra(letras[i], 60 * i, b, c);
+    b = b + 60;
+    c = c + 60;
+    a++;
+  }
+}
+
+function pizarra(palabra, x, x1, x2) {
+  lienzo.font = "bold 50px verdana";
+  lienzo.textAling = "start";
+  lienzo.textBaseline = "buttom";
+  lienzo.fillText(palabra, x, 50);
+  lienzo.moveTo(x1, 66);
+  lienzo.lineTo(x2, 66);
+  lienzo.lineWidth = 10;
+  lienzo.stroke();
+}
+
 let input = document.querySelector("#input");
-let input = document.querySelector("#input_2");
-let input = document.querySelector("#input_3");
+let canvas = document.getElementById("canvas");
+let lienzo = canvas.getContext("2d");
