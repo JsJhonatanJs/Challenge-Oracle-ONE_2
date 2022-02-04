c3152e92f4cddd14d26d5aa549675e9a01f62744
Jhonatan <jsjhonatanjsarroyo@gmail.com>
2022-02-03 19:53:02 -0500

    - Funcion de agregar cada guion. - Funcion de autocompletar el texto.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,34 @@
-function texto() {
-  let letras = input.value;
+function espacio() {
+  let letras = separador;
   let a = 1;
   let b = 0;
   let c = 44;
-  for (let i = 0; i < letras.length; i++) {
+  for (let i = 0; i < palabras.length; i++) {
     pizarra(letras[i], 60 * i, b, c);
     b = b + 60;
     c = c + 60;
     a++;
   }
+}
+
+function space() {
+  let espacios = " ";
+  for (let i = 1; i < palabras.length; i++) {
+    espacios = espacios + " ";
+  }
+  return espacios;
+}
+
+function texto() {
+  let letras_1 = input.value;
+
+  for (let i = 0; i < palabras.length; i++) {
+    if (letras_1 === palabras[i]) {
+      separador[i] = letras_1;
+    }
+  }
+  espacio();
+  input.value = "";
 }
 
 function pizarra(palabra, x, x1, x2) {
@@ -23,5 +43,9 @@ function pizarra(palabra, x, x1, x2) {
 }
 
 let input = document.querySelector("#input");
+let boton = document.querySelector("#boton");
 let canvas = document.getElementById("canvas");
 let lienzo = canvas.getContext("2d");
+boton.onclick = espacio;
+let palabras = "jhonatan";
+var separador = space().split("");
