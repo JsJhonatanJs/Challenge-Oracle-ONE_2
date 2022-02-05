5e92adab79f42ef63ab05e336f1fea4a0c48a1d9
Jhonatan <jsjhonatanjsarroyo@gmail.com>
2022-02-05 18:47:15 -0500

    - Centrado de todos los objetos - Ocultar botones y mostrar el juego

--- javascript.js
+++ javascript.js
@@ -4,11 +4,14 @@ function espacio() {
   let b = 0;
   let c = 44;
   for (let i = 0; i < palabras.length; i++) {
-    pizarra(letras[i], 60 * i, b, c);
+    pizarra(letras[i], 60 * i, b, c, 50, 66, 66);
     b = b + 60;
     c = c + 60;
     a++;
   }
+  input_1.style.display = "block";
+  divH_1.style.display = "block";
+  divH.style.display = "none";
 }
 
 function space() {
@@ -20,32 +23,56 @@ function space() {
 }
 
 function texto() {
-  let letras_1 = input.value;
-
+  let letras_1 = input_1.value.toLowerCase();
   for (let i = 0; i < palabras.length; i++) {
     if (letras_1 === palabras[i]) {
-      separador[i] = letras_1;
+      separador[i] = letras_1.toUpperCase();
     }
   }
   espacio();
-  input.value = "";
+  parrafo_1();
+  input_1.value = "";
 }
 
-function pizarra(palabra, x, x1, x2) {
+function parrafo_1() {
+  let cloud = separador.join("");
+  cloud = cloud.toLowerCase();
+  if (cloud === palabras) {
+    parrafo.innerHTML = "Ganaste";
+  }
+}
+
+function pizarra(palabra, x, x1, x2, hP, hL, hL2) {
   lienzo.font = "bold 50px verdana";
   lienzo.textAling = "start";
   lienzo.textBaseline = "buttom";
-  lienzo.fillText(palabra, x, 50);
-  lienzo.moveTo(x1, 66);
-  lienzo.lineTo(x2, 66);
+  lienzo.fillText(palabra, x, hP);
+  lienzo.moveTo(x1, hL);
+  lienzo.lineTo(x2, hL2);
   lienzo.lineWidth = 10;
   lienzo.stroke();
 }
 
+function divH_1_Click() {
+  input_1.focus();
+}
+
 let input = document.querySelector("#input");
+let input_1 = document.querySelector("#input_1");
 let boton = document.querySelector("#boton");
+let boton_1 = document.querySelector("#boton_");
+let parrafo = document.querySelector("#parrafo");
+let divH = document.querySelector("#divH");
+let divH_1 = document.querySelector("#divH_1");
 let canvas = document.getElementById("canvas");
 let lienzo = canvas.getContext("2d");
-boton.onclick = espacio;
-let palabras = "jhonatan";
+
+let arreglo = ["jhonatan", "alura", "challenge", "oracle"];
+let number = arreglo.length - 1;
+let numero = Math.round(Math.random() * number);
+let palabras = arreglo[numero];
+console.log(palabras);
 var separador = space().split("");
+boton.onclick = espacio;
+divH_1.style.display = "none";
+input_1.style.display = "none";
