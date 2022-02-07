06730de6abfb6541b8f3c3a1ff98c068056cebfb
Jhonatan <jsjhonatanjsarroyo@gmail.com>
2022-02-07 16:24:26 -0500

    - cambio de color al ahocado, letras, guiones - agregado letras equivocadas en rojo

--- canvas.js
+++ canvas.js
@@ -1,11 +1,12 @@
-let canvas = document.querySelector("#canvas");
-let lienzo = canvas.getContext("2d");
-
 function pizarra(palabra, x, x1, x2, hP, hL, hL2) {
   lienzo.font = "bold 50px verdana";
   lienzo.textAling = "start";
   lienzo.textBaseline = "buttom";
+  lienzo.fillStyle = "green";
   lienzo.fillText(palabra, x, hP);
+
+  lienzo.beginPath();
+  lienzo.strokeStyle = "blue";
   lienzo.moveTo(x1, hL);
   lienzo.lineTo(x2, hL2);
   lienzo.lineWidth = 10;
@@ -13,7 +14,17 @@ function pizarra(palabra, x, x1, x2, hP, hL, hL2) {
   lienzo.stroke();
 }
 
-function ahorcado(xi, xf, yi, yf) {
+function pizarraError(palabra, x, y) {
+  lienzo.font = "bold 50px verdana";
+  lienzo.textAling = "start";
+  lienzo.textBaseline = "buttom";
+  lienzo.fillStyle = "red";
+  lienzo.fillText(palabra, x, y);
+}
+
+function ahorcado(xi, xf, yi, yf, c) {
+  lienzo.beginPath();
+  lienzo.strokeStyle = c;
   lienzo.moveTo(xi, yi);
   lienzo.lineTo(xf, yf);
   lienzo.lineWidth = 10;
@@ -21,21 +32,37 @@ function ahorcado(xi, xf, yi, yf) {
   lienzo.stroke();
 }
 
-function ahorcadoC(cX, cY, r) {
+function ahorcadoC(cX, cY, r, c) {
   lienzo.beginPath();
+  lienzo.strokeStyle = c;
   lienzo.arc(cX, cY, r, 0, 2 * 3.14);
   lienzo.stroke();
 }
 
-ahorcado(10, 200, 490, 490);
-ahorcado(10, 100, 490, 450);
-ahorcado(200, 100, 490, 450);
-ahorcado(100, 100, 450, 10);
-ahorcado(100, 400, 10, 10);
-ahorcado(400, 400, 10, 80);
-ahorcadoC(400, 120, 40);
-ahorcado(400, 400, 160, 350);
-ahorcado(400, 310, 250, 182);
-ahorcado(400, 490, 250, 180);
-ahorcado(400, 310, 350, 420);
-ahorcado(400, 490, 350, 420);
+let canvas = document.querySelector("#canvas");
+let lienzo = canvas.getContext("2d");
+
+ahorcado(10, 200, 490, 490, "brown");
+ahorcado(10, 100, 490, 450, "brown");
+ahorcado(200, 100, 490, 450, "brown");
+
+ahorcado(100, 100, 450, 10, "brown");
+
+ahorcado(100, 400, 10, 10, "brown");
+
+ahorcado(400, 400, 10, 80, "brown");
+
+ahorcadoC(400, 120, 40, "black");
+ahorcado(380, 390, 110, 100, "black");
+ahorcado(380, 390, 100, 110, "black");
+ahorcado(410, 420, 110, 100, "black");
+ahorcado(410, 420, 100, 110, "black");
+ahorcado(410, 390, 140, 140, "black");
+
+ahorcado(400, 400, 160, 350, "black");
+
+ahorcado(400, 310, 250, 182, "black");
+ahorcado(400, 490, 250, 180, "black");
+
+ahorcado(400, 310, 350, 420, "black");
+ahorcado(400, 490, 350, 420, "black");
--- javascript.js
+++ javascript.js
@@ -3,32 +3,53 @@ function espacio() {
   let a = 240;
   let b = 240;
   let c = 280;
+
   for (let i = 0; i < palabras.length; i++) {
     pizarra(letras[i], a, b, c, 470, 490, 490);
     a = a + 60;
     b = b + 60;
     c = c + 60;
   }
+
   input_1.style.display = "block";
   divH_1.style.display = "block";
   divH.style.display = "none";
+  input_1.focus();
+}
+
+function errorLetras() {
+  let x = 500;
+  for (let i = 0; i < arregloError.length; i++) {
+    pizarraError(arregloError[i], x, 250);
+    x = x + 60;
+  }
 }
 
 function space() {
   let espacios = " ";
+
   for (let i = 1; i < palabras.length; i++) {
     espacios = espacios + " ";
   }
+
   return espacios;
 }
 
 function texto() {
   let letras_1 = input_1.value.toLowerCase();
+
   for (let i = 0; i < palabras.length; i++) {
     if (letras_1 === palabras[i]) {
       separador[i] = letras_1.toUpperCase();
     }
   }
+
+  let letraE = palabras.search(letras_1);
+  if (letraE === -1) {
+    arregloError.push(letras_1.toUpperCase());
+    errorLetras();
+  }
+  console.log(arregloError);
   espacio();
   parrafo_1();
   input_1.value = "";
@@ -37,6 +58,7 @@ function texto() {
 function parrafo_1() {
   let cloud = separador.join("");
   cloud = cloud.toLowerCase();
+
   if (cloud === palabras) {
     parrafo.innerHTML = "Ganaste";
   }
@@ -49,6 +71,7 @@ function divH_1_Click() {
 function numeroAleratorio() {
   let number = arreglo.length - 1;
   let numero = Math.round(Math.random() * number);
+
   palabras = arreglo[numero];
   separador = space().split("");
   espacio();
@@ -61,16 +84,22 @@ function agregarPalabra() {
 
 let input = document.querySelector("#input");
 let input_1 = document.querySelector("#input_1");
+
 let boton = document.querySelector("#boton");
 let boton_1 = document.querySelector("#boton_1");
+
 let parrafo = document.querySelector("#parrafo");
+
 let divH = document.querySelector("#divH");
 let divH_1 = document.querySelector("#divH_1");
 
 let arreglo = ["jhonatan", "alura", "challenge", "oracle"];
+let arregloError = [];
 let palabras = "";
 let separador = space().split("");
+
 boton.onclick = numeroAleratorio;
 boton_1.onclick = agregarPalabra;
+
 divH_1.style.display = "none";
 input_1.style.display = "none";
